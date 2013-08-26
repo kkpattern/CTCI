891947d3c0ee6e6d27971a44b6f521ba9650f1d4
Kai Zhang <kylerzhang11@gmail.com>
2013-08-26 16:05:47 +0800

    Added doublely linked list implementation.

--- chapter2/lists.c
+++ chapter2/lists.c
@@ -40,3 +40,42 @@ SinglyLinkedList *singly_linked_list_free(SinglyLinkedList *list) {
   }
   return NULL;
 }
+
+// Allocate a linked list node.
+LinkedList *linked_list_alloc(int value) {
+  LinkedList *node = (LinkedList *) malloc(sizeof(LinkedList));
+  node->prev = NULL;
+  node->next = NULL;
+  node->value = value;
+  return node;
+}
+
+// Append a node to the LinkedList.
+// @in list: The list to which the node is appended.
+// @in value: The value of the node.
+// @return: The new head of the LinkedList.
+LinkedList *linked_list_append(LinkedList *list, int value) {
+  if (NULL == list) return linked_list_alloc(value);
+
+  LinkedList *tail = list;
+  while (tail->next != NULL) tail = tail->next;
+
+  LinkedList *new_node = linked_list_alloc(value);
+  tail->next = new_node;
+  new_node->prev = tail;
+  return list;
+}
+
+// Free a LinkedList.
+// @in list: The list to free.
+// @return: NULL to clear the variable. E.g. 
+//   list = linked_list_free(list);
+LinkedList *linked_list_free(LinkedList *list) {
+  LinkedList *node = list;
+  while (NULL != node) {
+    LinkedList *tmp = node->next;
+    free(node);
+    node = tmp;
+  }
+  return NULL;
+}
--- chapter2/lists.h
+++ chapter2/lists.h
@@ -24,4 +24,28 @@ SinglyLinkedList *singly_linked_list_append(SinglyLinkedList *list, int value);
 SinglyLinkedList *singly_linked_list_free(SinglyLinkedList *list);
 
 
+typedef struct _LinkedList {
+  struct _LinkedList *prev;
+  struct _LinkedList *next;
+  int value;
+} LinkedList;
+
+
+// Allocate a linked list node.
+LinkedList *linked_list_alloc(int value);
+
+// Append a node to the LinkedList.
+// @in list: The list to which the node is appended.
+// @in value: The value of the node.
+// @return: The new head of the LinkedList.
+LinkedList *linked_list_append(LinkedList *list, int value);
+
+// Free a LinkedList.
+// @in list: The list to free.
+// @return: NULL to clear the variable. E.g. 
+//   list = linked_list_free(list);
+LinkedList *linked_list_free(LinkedList *list);
+
+
+
 #endif
