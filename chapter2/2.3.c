68dcee1b11847a78464ed80da6545b1b4a9676ee
Kai Zhang <kylerzhang11@gmail.com>
2013-08-26 17:15:59 +0800

    Solved 2.3.

---
+++ chapter2/2.3.c
@@ -0,0 +1,33 @@
+// Delete a node in the middle of a single linked list, given only access to
+// that node.
+#include <assert.h>
+#include <stddef.h>
+
+#include "lists.h"
+
+// Delete a node in the middle of a single linked list, given only access to
+// that node.
+// @in node: The given node.
+void delete_node(SinglyLinkedList *node) {
+  SinglyLinkedList *last_node = NULL;
+  while (NULL != node->next) {
+    node->value = node->next->value;
+    last_node = node;
+    node = node->next;
+  }
+  last_node->next = NULL;
+  singly_linked_list_free(node);
+}
+
+int main() {
+  SinglyLinkedList *list = NULL;
+  list = singly_linked_list_append(list, 1);
+  list = singly_linked_list_append(list, 2);
+  list = singly_linked_list_append(list, 3);
+  list = singly_linked_list_append(list, 4);
+  list = singly_linked_list_append(list, 5);
+  singly_linked_list_print(list);
+  delete_node(list->next->next);
+  singly_linked_list_print(list);
+  singly_linked_list_free(list);
+}
